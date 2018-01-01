0f4200d00cf06eeb8a0b3b83776046a9fa50b829
Rdyx <tiranico.pro@gmail.com>
2018-01-01 18:32:27 +0100

    Fixed bash for crontab

--- python/startCrawl.sh
+++ python/startCrawl.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/cryptogame/www/python
 echo "Commencement du script..."
 scrapy runspider crawlerCMC.py -s FEED_EXPORT_ENCODING=utf-8 -o currencies.json
 echo "update json fini"
