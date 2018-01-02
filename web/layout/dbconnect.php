c1f359b963ea56ca66ab83d87ed2694168258c14
Rdyx <tiranico.pro@gmail.com>
2018-01-02 03:10:11 +0100

    fixed unique visitor count bug

--- web/layout/dbconnect.php
+++ web/layout/dbconnect.php
@@ -10,7 +10,7 @@ $i = 0;
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-if(!isset($_SESSION['nick']) || $_SESSION['nick'] === 'Guest') {
+if(!isset($_SESSION['nick'])) {
     htmlspecialchars($_SESSION['nick'] = 'Guest');
     $sql = "UPDATE compteur SET com_visites = com_visites+1";
     $conn->query($sql);
